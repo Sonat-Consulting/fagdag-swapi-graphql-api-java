acdcf5becb098c56aa24df1cc3cd933c8bbb2e24
Olav Alexander Mjelde <olav.alexander.mjelde@sonat.no>
2021-08-27 14:19:15 +0200

    Fredrik saves the day

--- src/main/java/no/sonat/fagdag/graphql/swapiapi/services/SwapiClientImpl.java
+++ src/main/java/no/sonat/fagdag/graphql/swapiapi/services/SwapiClientImpl.java
@@ -24,9 +24,9 @@ public class SwapiClientImpl implements SwapiClient {
     private final String vehiclesUri;
 
     public SwapiClientImpl() {
-        filmsUri = "https://swapi.dev/api/films/%d/?format=json";
-        allFilmsUri = "https://swapi.dev/api/films/?format=json";
-        vehiclesUri = "https://swapi.dev/api/vehicles/%d/?format=json";
+        filmsUri = "http://192.168.74.149:8000/films/%d/?format=json";
+        allFilmsUri = "http://192.168.74.149:8000/films/?format=json";
+        vehiclesUri = "http://192.168.74.149:8000/vehicles/%d/?format=json";
     }
 
     @SneakyThrows
